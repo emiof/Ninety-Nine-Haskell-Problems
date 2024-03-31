4d3b99ea822470564692184844260983c6c866eb
emiof <emiliorfra@gmail.com>
2024-04-01 01:07:19 +1100

    Solve problems 26-27, Haskell

--- haskell/21_30.hs
+++ haskell/21_30.hs
@@ -27,7 +27,51 @@ myRange2 a b = take (b - a + 1) $ scanl (+) a $ repeat 1
 -- Problem 23: Extract a given number of randomly selected elements from a list.
 -- Time complexity: 
 -- Space complexity:
-randomSelect :: [a] -> Int -> []
+
+-- Problem 24: Lotto: Draw N different random numbers from the set 1..M.
+-- Time complexity: 
+-- Space complexity:
+
+-- Problem 25: Generate a random permutation of the elements of a list.
+-- Time complexity: 
+-- Space complexity:
+
+-- Problem 26: Generate combinations of K distinct objects chosen from the N elements of a list.
+-- Time complexity: 
+-- Space complexity:
+combinations :: (Eq a) => Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations k lst = concatMap (\(x:xs) -> [x:comb | comb <- combinations (k-1) xs]) $ take (length lst) $ iterate tail lst
+
+-- Time complexity: 
+-- Space complexity:
+combinations2 :: (Eq a) => Int -> [a] -> [[a]]
+combinations2 k lst = helper k lst []
+    where
+        helper :: (Eq a) => Int -> [a] -> [a] -> [[a]]
+        helper 0 _ currComb = [currComb]
+        helper _ [] _ = [] 
+        helper k (x:xs) currComb = helper (k-1) xs (x:currComb) ++ helper k xs currComb
+
+-- Time complexity: 
+-- Space complexity:
+combinations3 :: (Eq a) => Int -> [a] -> [[a]]
+combinations3 0 _ = [[]]
+combinations3 k lst = [lst !! i : x | i <- [0..(length lst - 1)], x <- combinations3 (k - 1) (drop (i + 1) lst)]
+
+-- Problem 27: Group the elements of a set into disjoint subsets.
+-- Time complexity: 
+-- Space complexity:
+applyAtIndex :: (Int -> Int) -> Int -> [Int] -> [Int]
+applyAtIndex f i  = zipWith (\i_ e -> if i_ == i then f e else e) [0..] 
+
+group :: [Int] -> [a] -> [[[a]]]
+group gps elems | length elems < sum gps =  []
+group gps [] = [replicate (length gps) []]
+group gps (x:xs) = (concatMap (\(i, g) -> if g > 0 then addToGroup i x $ group (applyAtIndex (subtract 1) i gps) xs else []) $ zip [0..] gps) ++ group gps xs
+    where
+        addToGroup :: Int -> a -> [[[a]]] -> [[[a]]]
+        addToGroup i e  =  map (\g -> zipWith (\i_ g_ -> if i_ == i then e:g_ else g_) [0..] g)
 
 -------------------------
 -- Problem n:
@@ -36,5 +80,4 @@ randomSelect :: [a] -> Int -> []
 
 main :: IO()
 main = do 
-    let x = 1 
-    print $ myRange2 5 10
+    print $ group [1, 2] [1, 2, 3, 4]
